f3e872e69ab1404575145573895049e7e2f2f0e4
Samuel Fomseca <samueljosef3@gmail.com>
2022-06-30 23:56:34 -0300

    Get by id, exception

--- backend/src/main/java/com/samueljf/crudclient/services/ClientService.java
+++ backend/src/main/java/com/samueljf/crudclient/services/ClientService.java
@@ -1,13 +1,17 @@
 package com.samueljf.crudclient.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.samueljf.crudclient.dto.ClientDTO;
 import com.samueljf.crudclient.entities.Client;
 import com.samueljf.crudclient.repositories.ClientRepository;
+import com.samueljf.crudclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -15,10 +19,18 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		
 		Page<Client> list = repository.findAll(pageRequest);
 		
 		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
+		return new ClientDTO(entity);
 	}
 }
